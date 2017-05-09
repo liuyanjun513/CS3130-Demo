6c7909a23ba657d0204deff29531650c0aa5439c
lenovo <yn739843@dal.ca>
2017-05-08 22:53:56 -0300

    Assignment1 done.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,39 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    EditText input;
+    TextView helloText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input=(EditText)findViewById(R.id.editText);
+        helloText=(TextView)findViewById(R.id.helloText);
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==button.getId()){
+            helloText.setText("");
+            String content=input.getText().toString();
+            helloText.setText(content);
+        }
+    }
+
 }
